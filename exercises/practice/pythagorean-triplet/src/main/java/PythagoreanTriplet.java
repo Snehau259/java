abc96ee7b57f42476ab90c6e82317669149a0b94
Sneha U <74255377+Snehau259@users.noreply.github.com>
2022-07-18 21:42:16 +0530

    Update PythagoreanTriplet.java

--- exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,10 +1,32 @@
-/*
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+public class pythagoreanTriplet {
+int a,b,c,n;
+void findpair(int n)
+{for (int i=1;i<n;i++)
+{
+	for(int j=i+1;j<n;j++)
+	{
+		for(int k=j+1;k<n;k++)
+		{
+			if (i+j+k==n)
+			{
+			//System.out.printf("%d,%d,%d\n",i,j,k);
+			if (i*i + j*j==k*k)
+			{
+				System.out.printf("{%d,%d,%d}\n",i,j,k);
+			}
+			}
+		}
+	}
+}
+	
+}
 
-Please remove this comment when submitting your solution.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pythagoreanTriplet obj=new pythagoreanTriplet();
+		obj.findpair(1000);
 
-*/
+	}
+
+}
