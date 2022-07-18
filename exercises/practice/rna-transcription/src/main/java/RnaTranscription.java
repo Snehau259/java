9b95f1fbe7a1c95a745de6fd13beb880db3b2266
Sneha U <74255377+Snehau259@users.noreply.github.com>
2022-07-18 15:30:41 +0530

    Update RnaTranscription.java

--- exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
+++ exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,32 @@
 class RnaTranscription {
 
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+    
+
+
+String rnaString="";
+	public String transcribe(String a)
+	{
+		for (int i =0;i<a.length();i++)
+		{
+			if (a.charAt(i)=='G')
+				rnaString+='C';
+			if (a.charAt(i)=='C')
+				rnaString+='G';
+			if (a.charAt(i)=='T')
+				rnaString+='A';
+			if (a.charAt(i)=='A')
+				rnaString+='U';
+		}
+	return rnaString;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rnaTranscription rna=new rnaTranscription();
+		System.out.println( rna.transcribe("ATGCAGT"));
+
+	}
 
 }
+
+
